24be1b80ff41f2c6bd1cdab91aaf052a850417d8
Erik Nakata <erik.nakata5@gmail.com>
2022-01-03 13:35:24 -0300

    Greeting a User with a customized message

--- lib/greeeting_service.ex
+++ lib/greeeting_service.ex
@@ -2,4 +2,8 @@ defmodule GreeetingService do
   def greet do
     "Then the system will reply with \"Hello my friend!\""
   end
+
+  def greet(name) when name == "Joe" do
+    "Hello Joe!"
+  end
 end
--- test/greeeting_service_test.exs
+++ test/greeeting_service_test.exs
@@ -7,4 +7,10 @@ defmodule GreeetingServiceTest do
       assert GreeetingService.greet() == "Then the system will reply with \"Hello my friend!\""
     end
   end
+
+  describe "When a User with the name Joe request a greeting message" do
+    test "Then the system will reply with a customized message that says: \"Hello Joe!\"" do
+      assert GreeetingService.greet("Joe") == "Hello Joe!"
+    end
+  end
 end
